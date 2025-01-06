b3cf125d6d1fb4c24f6df0b698c4e6d23515484c
yu-tomori <peacetomori1118@gmail.com>
2025-01-06 20:29:06 +0900

    initialize git

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,10 +6,15 @@ const Wrapper = styled.div`
     margin: var(--global-padding);
 `
 
+const Margin = styled.div`
+    height: 40px;
+`
+
 const Layout = ({children}) => {
     return (
         <Wrapper>
             <Breadcrumbs />
+            <Margin />
             {children}
         </Wrapper>
     )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,56 @@
 import React from "react";
+import styled from "styled-components";
 import { NavLink } from "react-router-dom";
 import Layout from "../components/Layout.js";
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+`
+
+const LinksContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+`
+
+const SectionTitle = styled.p`
+    font-size: var(--text-large);
+    color: var(--gray);
+`
+
+const Link = styled.p`
+    color: var(--blue);
+    font-size: var(--text-large);
+    text-decoration: underline;
+    text-decoration-color: var(--blue);
+`
 
 const Home = () => {
     return (
         <Layout>
-            <NavLink to="posts">posts</NavLink>
+            <Container>
+            <SectionTitle>MENU</SectionTitle>
+            <LinksContainer>
+                <NavLink to="posts">
+                    <Link>posts</Link>
+                </NavLink>
+                <NavLink to="posts">
+                    <Link>biography</Link>
+                </NavLink>
+            </LinksContainer>
+
+            <SectionTitle>ACCOUNTS</SectionTitle>
+            <LinksContainer>
+                <NavLink to="posts">
+                    <Link>twitter</Link>
+                </NavLink>
+                <NavLink to="posts">
+                    <Link>github</Link>
+                </NavLink>
+            </LinksContainer>
+            </Container>
         </Layout>
     )
 }
