a537799450732e5a816d537df5119443b8c4d937
yu-tomori <peacetomori1118@gmail.com>
2025-01-13 16:08:52 +0900

    customize markdown compoents

--- src/components/markdown/index.js
+++ src/components/markdown/index.js
@@ -3,15 +3,48 @@
 */
 import React from 'react'
 
-const li = ({ children }) => (
-    <li style={{
-        color: 'red',
-        marginLeft: '20px'
-    }}>{children}</li>
+const margins = {
+    marginTop: '10px',
+    marginBottom: '10px'
+}
+
+const h1 = ({ node, ...props }) => (
+    <h1 style={{
+        fontWeight: 'normal',
+        ...margins
+    }} {...props} />
 )
 
+const h2 = ({ node, ...props }) => (
+    <h2 style={{
+        fontWeight: 'normal',
+        ...margins
+    }} {...props} />
+)
+
+const h3 = ({ node, ...props }) => (
+    <h3 style={{
+        fontWeight: 'normal',
+        ...margins
+    }} {...props} />
+)
+
+const h4 = ({ node, ...props }) => (
+    <h4 style={{
+        fontWeight: 'normal',
+        ...margins
+    }} {...props} />
+)
+
+const li = ({ node, ...props }) => (
+    <li style={{
+        marginLeft: '20px'
+    }} {...props} />
+)
+
+
 const mdComponents = {
-    li: li
+    li, h1, h2, h3, h4
 }
 
 export default mdComponents
