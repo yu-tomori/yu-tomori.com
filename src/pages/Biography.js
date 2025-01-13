fd7767989f267514dfa81fae30bf0da33ab024a3
yu-tomori <peacetomori1118@gmail.com>
2025-01-13 17:30:05 +0900

    add contents in /biography

--- src/pages/Biography.js
+++ src/pages/Biography.js
@@ -1,10 +1,31 @@
 import React from "react";
 import Layout from "../components/Layout";
+import styled from "styled-components";
+
+
+const Li =  styled.li`
+    margin-left: 20px;
+`
 
 const Biography = () => {
     return (
         <Layout>
-            <p>under construction</p>
+            <ul>
+                <Li>Yu Tomori</Li>
+                <Li>Born in Okinawa, now in Kamakura.</Li>
+                <Li>Product Manager at PortX Inc., working on Logistics Spend Management.</Li>
+                <Li>
+                    I worked 4 years as a software engineer before this job.
+                </Li>
+                <Li>
+                    My passionates
+                    <ul>
+                        <Li>learning software design and development</Li>
+                        <Li>exercising, mainly running on road or trail</Li>
+                        <Li>cooking. I like Italian dishes, selecting wine as well.</Li>
+                    </ul>
+                </Li>
+            </ul>
         </Layout>
     )
 }
