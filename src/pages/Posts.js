9bde4e5609c1b3ba1ff8d6472105efe646b531d6
yu-tomori <peacetomori1118@gmail.com>
2025-01-13 14:54:12 +0900

    style /posts

--- src/pages/Posts.js
+++ src/pages/Posts.js
@@ -6,44 +6,47 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://sravcubcggcbhozcsyvo.supabase.co'
 const supabase = createClient(supabaseUrl, process.env.SUPABASE_KEY)
 
-const PostsContainer = styled.div`
-    padding: 20px;
-    background-color: #f9f9f9;
-    border-radius: 8px;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-`;
-
 const PostsList = styled.ul`
     list-style-type: none;
-    padding: 0;
 `;
 
 const PostItem = styled.li`
     padding: 10px;
     margin-bottom: 10px;
-    background-color: #fff;
-    border: 1px solid #ddd;
     border-radius: 4px;
     transition: background-color 0.3s;
-
     &:hover {
         background-color: #f1f1f1;
     }
 `;
 
+const Title = styled.p`
+    color: var(--blue);
+    text-decoration: underline;
+    font-size: var(--text-medium);
+`
+
+const Date = styled.p`
+    color: var(--black);
+    font-size: var(--text-small);
+`
+
 // for suspense boundary
 const Inner = ({ data }) => {
     const {data: posts}= use(data)
+
+    const formatDate = (date) => { // rfc3339
+        return date.slice(0, 10).replaceAll('-', '/')
+    }
     return (
-        <PostsContainer>
-            <PostsList>
-                {posts.map((post) => (
-                    <PostItem key={post.id}>
-                        {post.id} {post.created_at}
-                    </PostItem>
-                ))}
-            </PostsList>
-        </PostsContainer>
+        <PostsList>
+            {posts.map((post) => (
+                <PostItem key={post.id}>
+                    <Title>{post.title}</Title>
+                    <Date>{formatDate(post.created_at)}</Date>
+                </PostItem>
+            ))}
+        </PostsList>
     )
 }
 
